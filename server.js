ba8e253da1706fe70a6dddc25db41c452dc1828c
Sascha Monteiro <sascha.monteiro@gmail.com>
2017-05-11 08:55:48 +0000

    implemented user story 3

--- server.js
+++ server.js
@@ -17,40 +17,56 @@ var optionsget = {
 //https://www.googleapis.com/customsearch/v1?key=AIzaSyDEWyftPNTCNRD5J37zeTWnuRtGt-9ozWM&cx=005819915415754972069:kmyahl68k9k&q=funny%20cat
 app.get('/api/imagesearch/:search', function(req, res1) {
     var offset = parseInt(req.query.offset, 10)
+    var search = decodeURI(req.params.search)
     console.info('search', req.params.search)
     console.info('offset', offset)
-    optionsget.path = optionsget.path + '&q=' + req.params.search
-    if(typeof offset == 'number') {
+    optionsget.path = optionsget.path + '&q=' + encodeURI(req.params.search)
+    if(!isNaN(offset) && typeof offset == 'number') {
          optionsget.path = optionsget.path + '&start=' + offset
     }
+    MongoClient.connect(mongodburl, function(err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server [' + mongodburl + ']. Error:', err)
+    }
+    else {
+        var collection = db.collection(collectionName)
+        collection.insert({
+                term: search,
+                when: new Date()
+              }, function(err, records) {
+                if (err) {
+                  console.log('error', err)
+                }
+            
+            db.close()
+          })
+    }})
     console.info('options', optionsget)
     var buf = '';
     var reqGet = https.request(optionsget, function(res) {
-        console.log("statusCode: ", res.statusCode);
-        // uncomment it for header details
-    //  console.log("headers: ", res.headers);
-     
-     
         res.on('data', function(d) {
             buf += d
         });
-        res.on('end', function () {
-            var ret = JSON.parse(buf)
-            var items = ret.items
-            var returnObject = []
-            items.forEach(function(data, idx){
-                var item = {
-                    url: data.pagemap.imageobject[0].url,
-                    snippet: data.snippet,
-                    thumbnail: data.pagemap.cse_thumbnail[0].src,
-                    context: data.link
-                }
-                console.info('item', item)
-                returnObject.push(item)
-            })
-            console.log('BODY: ' + ret);
-            console.log('BODY: ' + ret.items.length);
-            res1.send(returnObject)
+        res.on('end', function (err) {
+            if(err){
+                console.log('error', err)
+            }else{
+                console.log('buf', buf)
+                var ret = JSON.parse(buf)
+                var items = ret.items
+                var returnObject = []
+                items.forEach(function(data, idx){
+                    var item = {
+                        url: data.pagemap.imageobject ? data.pagemap.imageobject[0].url : data.pagemap.cse_thumbnail[0].src,
+                        snippet: data.snippet,
+                        thumbnail: data.pagemap.cse_thumbnail[0].src,
+                        context: data.link
+                    }
+                    console.info('item', item)
+                    returnObject.push(item)
+                })
+                res1.send(returnObject)
+            }
           });
     });
      
@@ -60,6 +76,34 @@ app.get('/api/imagesearch/:search', function(req, res1) {
     });
     
     
+})
+
+app.get('/api/latest/imagesearch', function(req, res1) {
+    console.info('latest')
+    MongoClient.connect(mongodburl, function(err, db) {
+      if (err) {
+        console.log('Unable to connect to the mongoDB server [' + mongodburl + ']. Error:', err)
+      }
+      else {
+        var collection = db.collection(collectionName)
+        
+        
+        
+        collection
+            .find({}, {"_id": 0, "term": 1, "when": 1}, {'limit': 10})
+            .sort({'when': -1})
+            .toArray( function(err, d) {
+              if (err) {
+                console.log('error', err)
+              }
+              else {
+                console.log('doc', d)
+                res1.send(JSON.stringify(d))
+              }
+              db.close()
+            })
+      }
+    })
 })
 
 app.listen(port, function() {
